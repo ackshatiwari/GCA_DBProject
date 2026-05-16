64e1ac4d90d025ed980e25b78156e24f4913d4b5
Ackshat Tiwari <ackshat.tiwari@gmail.com>
2026-05-15 21:19:13 -0400

    found scraper URL template which will be used for web scraping

---
+++ backend/scripts/web_scrape_script.py
@@ -0,0 +1,4 @@
+# web scrapes every month for all sites and organisms, and checks the
+# Example URL
+# https://api.cleanwaterhub.org/v1/data/protocol_vasos_rocky_bottom?q=%7B%22filters%22:%5B%7B%22name%22:%22site_id%22,%22op%22:%22eq%22,%22val%22:%2211234%22%7D,%7B%22or%22:%5B%7B%22name%22:%22has_been_deleted%22,%22op%22:%22neq%22,%22val%22:%22true%22%7D,%7B%22name%22:%22has_been_deleted%22,%22op%22:%22is_null%22%7D%5D%7D,%7B%22or%22:%5B%7B%22name%22:%22has_been_archived%22,%22op%22:%22neq%22,%22val%22:%22true%22%7D,%7B%22name%22:%22has_been_archived%22,%22op%22:%22is_null%22%7D%5D%7D%5D,%22order_by%22:%5B%7B%22field%22:%22survey_date%22,%22direction%22:%22desc%22%7D%5D%7D&results_per_page=200
+
--- backend/tests/web_scrape_test.py
+++ backend/tests/web_scrape_test.py
@@ -1,11 +1,67 @@
 import requests
+import json
+import urllib.parse
 from bs4 import BeautifulSoup
 
+from pathlib import Path
 
-res = requests.get('https://api.cleanwaterhub.org/v1/data/protocol_vasos_rocky_bottom/13684')
-if res.status_code == 200:
-    data = res.json()
-    soup = BeautifulSoup(res.text, 'html.parser')
-    print(soup.prettify())
-else:
-    print(f"Failed to fetch data: {res.status_code}")
+# Get the directory of the currently running script
+current_dir = Path(__file__).parent
+file_path = current_dir / "web_scrape_output.html"
+
+site_id = "10994"
+
+query = {
+    "filters": [
+        {
+            "name": "site_id",
+            "op": "eq",
+            "val": site_id
+        },
+        {
+            "or": [
+                {
+                    "name": "has_been_deleted",
+                    "op": "neq",
+                    "val": "true"
+                },
+                {
+                    "name": "has_been_deleted",
+                    "op": "is_null"
+                }
+            ]
+        },
+        {
+            "or": [
+                {
+                    "name": "has_been_archived",
+                    "op": "neq",
+                    "val": "true"
+                },
+                {
+                    "name": "has_been_archived",
+                    "op": "is_null"
+                }
+            ]
+        }
+    ],
+    "order_by": [
+        {
+            "field": "survey_date",
+            "direction": "desc"
+        }
+    ]
+}
+
+encoded_query = urllib.parse.quote(json.dumps(query))
+
+url = f"https://api.cleanwaterhub.org/v1/data/protocol_vasos_rocky_bottom?q={encoded_query}&results_per_page=200"
+
+print(url)
+
+response = requests.get(url)
+data = response.json()
+soup = BeautifulSoup(response.text, 'html.parser')
+with open(file_path, 'w', encoding='utf-8') as f:
+    f.write(soup.prettify())
+
